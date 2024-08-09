c2364a8e1647afb44f742b53937f83bb07ae60d1
Priyaank Chhadwa <priyaank29@gmail.com>
2024-08-09 12:38:50 -0400

    changed from dropdown to button toggle

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -14,24 +14,40 @@ import {
 
 export function ModeToggle() {
   const { setTheme } = useTheme()
+  let isDark: boolean = true;
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="bg-transparent border-0">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    // <DropdownMenu>
+    //   <DropdownMenuTrigger asChild>
+    //     <Button variant="outline" size="icon" className="bg-transparent border-0">
+    //       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+    //       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    //       <span className="sr-only">Toggle theme</span>
+    //     </Button>
+    //   </DropdownMenuTrigger>
+    //   <DropdownMenuContent align="end">
+    //     <DropdownMenuItem onClick={() => setTheme("light")}>
+    //       Light
+    //     </DropdownMenuItem>
+    //     <DropdownMenuItem onClick={() => setTheme("dark")}>
+    //       Dark
+    //     </DropdownMenuItem>
+    //   </DropdownMenuContent>
+    // </DropdownMenu>
+    
+
+    <Button 
+      variant="outline"
+      size="icon"
+      className="bg-transparent border-0"
+      onClick={() => {
+        isDark = !isDark;
+        setTheme(isDark ? "dark" : "light");
+      }}
+    >
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   )
 }
